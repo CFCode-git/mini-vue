5f6da692083ac19be09ac51725e981943462c124
周啟尧 <435184766@qq.com>
2022-12-01 17:43:36 +0800

    实现ref(3)

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -1,20 +1,31 @@
 import { trackEffect, triggerEffect, isTracking } from './effect'
+import { isObject } from '../shared/index'
+import { reactive } from './reactivity'
+
 class RefImpl {
   private _value: any
   private deps
+  private _rawValue
+
   constructor(value) {
-    this._value = value
+    this._rawValue = value
+    this._value = isObject(value) ? reactive(value) : value
     this.deps = new Set()
   }
+
   get value() {
+    // 收集依赖
     if (isTracking()) {
       trackEffect(this.deps)
     }
     return this._value
   }
   set value(newValue) {
-    if (!Object.is(newValue, this._value)) {
-      this._value = newValue
+    // 只有值变化的时候才触发依赖
+    if (!Object.is(newValue, this._rawValue)) {
+      this._value = isObject(newValue) ? reactive(newValue) : newValue
+      this._rawValue = newValue
+      // 触发依赖
       triggerEffect(this.deps)
     }
   }
--- src/reactivity/test/ref.spec.ts
+++ src/reactivity/test/ref.spec.ts
@@ -28,10 +28,9 @@ describe('ref', () => {
     expect(calls).toBe(2)
   })
 
-  it.skip('should make nested properties reactive', () => {
-    const a = ref({
-      count: 1
-    })
+  it('should make nested properties reactive', () => {
+    // 这里返回的 a 是 一个 {value: Proxy} 对象，其中的 Proxy target 是 {count: 1}
+    const a = ref({ count: 1 })
     let dummy
     effect(() => {
       dummy = a.value.count
@@ -39,7 +38,23 @@ describe('ref', () => {
 
     expect(dummy).toBe(1)
 
-    a.value.count = 2
+    // 下面这种写法不会触发 refImpl 的 setter
+    // a.value.count = 2 // 第一种方式
+    // 当 ref 接受一个对象的时候，会直接返回一个被包裹的 reactive Proxy 
+    // a.value 触发 refImpl 的 getter，拿到 reactive Proxy，后面就是 reactive 的流程
+    // 后续 a.value.count = 2 触发 Proxy 的 setter(或getter)
+    // 从 targetMap > depsMap > dep 中取出依赖执行
+
+    // 下面这种写法才会触发 refImpl 的 setter，
+    a.value = { count: 2 } // 第二种方式
+    // 这里直接改了 refImpl 的 _value 的值，传入一个新对象
+    // 在初始化 refImpl 的时候存储了 原始对象 rawValue
+    // 当对比发现两者不相同，此时会重新对传入的新对象进行 reactive 处理，
+    // 同时将 rawValue 修改为传入的新对象.
+    // 最后触发依赖
+
+    // 当 refImpl 是一个对象的时候，effect 会在 refImpl.deps 和 targetMap[target][key] 分别存储一份，前者在第二种方式中触发，后者在第一种方式触发。
+
     expect(dummy).toBe(2)
   })
 })
