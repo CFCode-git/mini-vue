a68384e5bbf09b8f9394b95b3ae0169675e6e009
周啟尧 <435184766@qq.com>
2022-12-05 09:26:52 +0800

    实现proxyRefs

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -6,6 +6,7 @@ class RefImpl {
   private _value: any
   private deps
   private _rawValue
+  public __v_isRef = true
 
   constructor(value) {
     this._rawValue = value
@@ -33,4 +34,29 @@ class RefImpl {
 
 export function ref(value) {
   return new RefImpl(value)
+}
+
+export function isRef(ref) {
+  return !!ref.__v_isRef
+}
+
+export function unRef(ref) {
+  return isRef(ref) ? ref.value : ref
+}
+
+export function proxyRefs(objWithRef) {
+  return new Proxy(objWithRef, {
+    get(target, key) {
+      return unRef(Reflect.get(target, key))
+    },
+    set(target, key, value) {
+      if (isRef(target[key]) && !isRef(value)) {
+        // target[key].value 修改原对象的值，触发 ReefImpl setter
+        return (target[key].value = value)
+      } else {
+        // 直接替换
+        return Reflect.set(target, key, value)
+      }
+    }
+  })
 }
--- src/reactivity/test/ref.spec.ts
+++ src/reactivity/test/ref.spec.ts
@@ -1,5 +1,6 @@
 import { effect } from '../effect'
-import { ref } from '../ref'
+import { reactive } from '../reactivity'
+import { isRef, proxyRefs, ref, unRef } from '../ref'
 
 describe('ref', () => {
   it('happy path', () => {
@@ -40,7 +41,7 @@ describe('ref', () => {
 
     // 下面这种写法不会触发 refImpl 的 setter
     // a.value.count = 2 // 第一种方式
-    // 当 ref 接受一个对象的时候，会直接返回一个被包裹的 reactive Proxy 
+    // 当 ref 接受一个对象的时候，会直接返回一个被包裹的 reactive Proxy
     // a.value 触发 refImpl 的 getter，拿到 reactive Proxy，后面就是 reactive 的流程
     // 后续 a.value.count = 2 触发 Proxy 的 setter(或getter)
     // 从 targetMap > depsMap > dep 中取出依赖执行
@@ -56,5 +57,49 @@ describe('ref', () => {
     // 当 refImpl 是一个对象的时候，effect 会在 refImpl.deps 和 targetMap[target][key] 分别存储一份，前者在第二种方式中触发，后者在第一种方式触发。
 
     expect(dummy).toBe(2)
+  })
+
+  it('isRef', () => {
+    const a = ref(1)
+    const b = ref({ foo: 2 })
+    const c = reactive({
+      foo: 3
+    })
+    expect(isRef(a)).toBe(true)
+    expect(isRef(b)).toBe(true)
+
+    expect(isRef(1)).toBe(false)
+    expect(isRef(c)).toBe(false)
+
+    expect(isRef(b)).toBe(true)
+  })
+  it('unRef', () => {
+    const a = ref(1)
+
+    expect(unRef(a)).toBe(1)
+    expect(unRef(1)).toBe(1)
+  })
+  it('proxyRefs', () => {
+    // 常用于 template, 在 vue3 中, template 可以直接使用 ref 的值，而不需要 .value
+    const user = {
+      age: ref(10),
+      name: 'this is string!',
+      number: ref(5)
+    }
+
+    const proxyUser = proxyRefs(user)
+
+    // get
+    expect(proxyUser.age).toBe(10)
+    expect(user.age.value).toBe(10)
+
+    // set, Condition One: 修改原对象的值, 触发 RefImpl setter
+    proxyUser.age = 100
+    expect(proxyUser.age).toBe(100)
+    expect(user.age.value).toBe(100)
+
+    // set, Condition Two: 直接替换
+    proxyUser.name = 'I know!'
+    proxyUser.number = ref(50)
   })
 })
