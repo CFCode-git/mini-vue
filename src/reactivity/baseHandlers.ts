26e26106c420842fa288d8f3579b97cc2964d71b
周啟尧 <435184766@qq.com>
2022-12-30 19:26:43 +0800

    update: 重新理解shallowReadOnly

--- src/reactivity/baseHandlers.ts
+++ src/reactivity/baseHandlers.ts
@@ -1,16 +1,18 @@
 import { extend, isObject } from '../shared'
 import { track, trigger } from './effect'
-import { reactive, ReactiveFlags, readonly, shallowReadonly } from './reactivity';
+import { reactive, ReactiveFlags, readonly, shallowReadonly } from './reactivity'
 
 // 这里是为了复用 getter 函数，而不需要每次创建 proxy 的时候都创建一个新的 getter
 const get = createGetter()
 const set = createSetter()
 const readonlyGet = createGetter(true)
-const shallowReadonlyGet = createGetter(true,true)
+const shallowReadonlyGet = createGetter(true, true)
 
+// 默认：不是 readOnly, 不是 shallow : get() 可以获取值，并且做依赖收集
+// 是 readOnly, 不是 shallow : get() 可以获取值，不做依赖收集，同时递归下去，对象深层同样可以获取值，但是不做依赖收集
+// 是 readOnly, 是 shallow : get() 同样获取值，不做依赖收集。
 function createGetter(isReadonly = false, shallow = false) {
   return function get(target, key) {
-
     // 处理 isReadonly 和 isReactive 的调用
     if (key === ReactiveFlags.IS_REACTIVE) {
       return !isReadonly
@@ -20,12 +22,14 @@ function createGetter(isReadonly = false, shallow = false) {
 
     let res = Reflect.get(target, key)
 
-    if(shallow){
+    // 如果是 shallow 直接返回结果
+    if (shallow) {
       return res
     }
 
-    // 如果是shallow, 不需要递归对嵌套对象执行
-    if(isObject(res)){
+    // 如果是shallow, 不需要递归对嵌套对象执行,深层的对象不做proxy代理了。
+    // 如果不是shallow, 且返回的结果仍然是一个对象，根据是否 readOnly 对这个深层的对象继续做处理
+    if (isObject(res)) {
       return isReadonly ? readonly(res) : reactive(res)
     }
 
@@ -60,8 +64,8 @@ export const readonlyHandlers = {
   }
 }
 
-
 // 通过 extends(Object.assign) 改写 readonlyHandlers 的 getter 方法
-export const shallowReadonlyHandlers = extend({},readonlyHandlers,{
+// shallow 的第一层是 readonly, 但第二层往后的嵌套对象是普通对象, 普通对象不走 proxy 代理
+export const shallowReadonlyHandlers = extend({}, readonlyHandlers, {
   get: shallowReadonlyGet
 })
